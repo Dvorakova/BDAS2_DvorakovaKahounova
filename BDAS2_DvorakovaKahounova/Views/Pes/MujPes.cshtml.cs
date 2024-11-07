b37c1d4e0f3cea8fb3e6aa92d296c09c1d84dee0
Klára Kahounová <kahounova.klarka@seznam.cz>
2024-11-07 11:56:47 +0100

    stránka Můj účet

---
+++ BDAS2_DvorakovaKahounova/Views/Pes/MujPes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BDAS2_DvorakovaKahounova.Views.Pes
+{
+    public class MujPesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
