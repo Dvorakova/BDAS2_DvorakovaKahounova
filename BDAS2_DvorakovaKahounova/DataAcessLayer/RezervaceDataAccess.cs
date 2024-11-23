8733c9b47711ba2ebb80a8dc9e2a2adcae51c71f
Klára Kahounová <kahounova.klarka@seznam.cz>
2024-11-23 16:44:38 +0100

    rezervace + RezervaceDataAccess

--- BDAS2_DvorakovaKahounova/DataAcessLayer/RezervaceDataAccess.cs
+++ BDAS2_DvorakovaKahounova/DataAcessLayer/RezervaceDataAccess.cs
@@ -1,4 +1,6 @@
-﻿namespace BDAS2_DvorakovaKahounova.DataAcessLayer
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace BDAS2_DvorakovaKahounova.DataAcessLayer
 {
     public class RezervaceDataAccess
     {
@@ -10,5 +12,29 @@
         }
 
         //pro práci s rezervacemi (s databází)
+
+        //pridano KK
+        public void VytvorRezervaci(int pesId, int osobaId, DateTime datumRezervace)
+        {
+            using (var con = new OracleConnection(_connectionString))
+            {
+                con.Open();
+
+                // Zavolání uložené procedury VYPISREZERVACE
+                using (var cmd = new OracleCommand("VYPISREZERVACE", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    // Přidání parametrů pro proceduru
+                    cmd.Parameters.Add(new OracleParameter("pPesId", pesId));
+                    cmd.Parameters.Add(new OracleParameter("pOsobaId", osobaId));
+                    cmd.Parameters.Add(new OracleParameter("pDatumRezervace", datumRezervace));
+
+                    // Spuštění procedury
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
--- BDAS2_DvorakovaKahounova/Models/Rezervace.cs
+++ BDAS2_DvorakovaKahounova/Models/Rezervace.cs
@@ -2,6 +2,11 @@
 {
     public class Rezervace
     {
-
+        //pridano KK
+        public int IdRezervace { get; set; }
+        public int IdPsa { get; set; }
+        public DateTime Datum { get; set; }
+        public int? IdAdopce { get; set; }
+        public int RezervatorIdOsoba { get; set; }
     }
 }
